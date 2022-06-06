bfd0eea94a2cfc6c6e31e5e956bb173a87f7e690
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-06-06 22:58:53 +0800

    [ADD] Add required pages and Navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    const logoLink = "https://img.icons8.com/external-flaticons-lineal-color-flat-icons/96/undefined/external-fashion-fashion-week-flaticons-lineal-color-flat-icons.png"
+</script>
+
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/Homepage">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Items
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="/Apparel">Apparel</a></li>
+                        <li><a class="dropdown-item" href="/Logos">Logos</a></li>
+                        <li><a class="dropdown-item" href="/Posters">Posters</a></li>
+                        <li><a class="dropdown-item" href="/Brands">Brands/Menu</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/ContactUs">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/AboutUs">About Us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,8 @@
 <script>
   import "../app.scss";
+  import "../main.css"
+;  import Navbar from "../lib/components/Navbar.svelte";
 </script>
 
+<Navbar/>
 <slot />
